4a6c491cd9b16289e8764351ed9218d16c69698a
Emma Navarro <elnavarro55@gmail.com>
2020-02-06 23:22:09 +0000

    alphabet reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - display alphabet in reverse and lowercase
+*Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
