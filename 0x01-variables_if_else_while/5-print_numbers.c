6cb9680efd603b19dffeb0a71e14867133e6bab8
Emma Navarro <elnavarro55@gmail.com>
2020-02-06 22:50:00 +0000

    numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - display all single digits of base 10
+*Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+	printf("%i", n);
+	}
+	putchar('\n');
+	return (0);
+}
