5eb66f460fc8b871f400373f7be6713ed85f4945
Emma Navarro <elnavarro55@gmail.com>
2020-02-12 03:46:27 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * add - add two integers
+ *
+ * @x: first integer parameter.
+ *
+ * @y: second integer parameter.
+ *
+ * Return: z integer addition.
+ */
+int add(int x, int y)
+{
+	int z = x + y;
+
+	return (z);
+}
