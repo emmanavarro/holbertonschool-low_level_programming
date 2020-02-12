5a9ffd8ae5fd4a1e78cb0b0a01273cde5b2ea7f7
Emma Navarro <elnavarro55@gmail.com>
2020-02-12 01:51:21 +0000

    prints Holberton with new line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -3,15 +3,17 @@
  * main - print the word Holberton
  *
  * Return: Always 0 (success)
-*/
- int main(void)
- {
- 	char array[9]="Holberton";
- 	int n=0;
- 	for (n = 0; n <=8; n++)
- 	{
- 		_putchar(array[n]);
- 	}
- 	_putchar('\n');
- 	return (0);
- }
+ */
+int main(void)
+{
+	char array[9] = "Holberton";
+	int n = 0;
+
+	for (n = 0; n <= 8; n++)
+	{
+		_putchar(array[n]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
