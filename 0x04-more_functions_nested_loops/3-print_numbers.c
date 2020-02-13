212d60f8557e3855e80be2ffe001c9ccec9b1b3a
Emma Navarro <elnavarro55@gmail.com>
2020-02-13 20:19:12 +0000

    add function prints 0 to 9 with new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints from 0 to 9.
+ * @i
+ * Return: i.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
