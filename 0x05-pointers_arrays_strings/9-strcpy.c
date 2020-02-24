700fe2c567bf8247c780149c07e2694e5de27ac8
emmanavarro <elnavarro55@gmail.com>
2020-02-24 02:02:25 +0000

    copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - function that copies a string
+ * @dest: pointer of destination
+ * @src: string to copy
+ * Return: pointer to dest
+ **/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
