d366b31e7cfd0bfda1cab95ec38ba2cfa2c20355
emmanavarro <elnavarro55@gmail.com>
2020-02-27 03:18:11 +0000

    upload task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - calls the function to calculate the square.
+ * @n: variable.
+ * Return: value.
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (calculate_sqrt(0, n));
+}
+
+/**
+ * calculate_srt - calculates the square of a number.
+ * @i: variable.
+ * @n: variable.
+ * Return: -1.
+ */
+
+int calculate_sqrt(int i, int n)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i < n)
+	{
+		return (calculate_sqrt(i + 1, n));
+	}
+	else
+	{
+		return (-1);
+	}
+}
