1c734b3cc9c821c00aaa4af6c2323cb53933dc0b
emmanavarro <elnavarro55@gmail.com>
2020-02-27 04:19:51 +0000

    upload holberton.h

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -9,7 +9,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int calculate_sqrt(int i, int n);
 int is_prime_number(int n);
-int is_palindrome(char *s);
+int prime(int i, int n);
 int _putchar(char c);
 
 #endif
