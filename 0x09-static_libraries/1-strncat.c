3292fcb4865908a77d86356ed8efee6a220c24dd
emmanavarro <elnavarro55@gmail.com>
2020-02-29 16:26:26 +0000

    upload library

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -9,13 +9,14 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i = 0; 
+	int j;
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
 		dest[i + j] = src[j];
 
-	
+
 	}
 	return (dest);
 }
