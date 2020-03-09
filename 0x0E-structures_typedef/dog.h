44731c17528d6743ccc2212d6582d493df6aac97
Emma Navarro <elnavarro55@gmail.com>
2020-03-09 21:45:27 +0000

    upload dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MACRO_DOG
+#define MACRO_DOG
+
+/**
+  * struct dog - dog structure.
+  * @name: dog's name.
+  * @age: dog's age.
+  * @owner: dog's owner.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*Struct MACRO_DOG*/
