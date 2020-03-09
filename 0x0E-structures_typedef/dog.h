85b07cfb92f9cfb518d482d99b7e6ad7f7a7a9a8
Emma Navarro <elnavarro55@gmail.com>
2020-03-09 23:30:27 +0000

    upload 1 dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,11 +7,19 @@
   * @age: dog's age.
   * @owner: dog's owner.
   */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif /*Struct MACRO_DOG*/
