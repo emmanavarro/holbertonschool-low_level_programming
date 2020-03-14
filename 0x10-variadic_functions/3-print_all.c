ccfa70dd5d4dc2beb5077e6a8f67d45f722ee62f
Emma Navarro <elnavarro55@gmail.com>
2020-03-14 00:59:42 +0000

    Upload task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,86 @@
+#include "variadic_functions.h"
+
+/**
+ * add_char - print a character.
+ * @list: list of arguments.
+ */
+
+void add_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+ * add_int - print an int.
+ * @list: list of arguments.
+ */
+
+void add_int(va_list list)
+{
+	printf("%i", va_arg(list, int));
+}
+
+
+/**
+ * add_float - print a float.
+ * @list: list of arguments.
+ */
+
+void add_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+
+/**
+ * add_str- print a string.
+ * @list: list of arguments.
+ */
+
+void add_str(va_list list)
+{
+	printf("%s", va_arg(list, char*));
+}
+
+/**
+ * print_all - prints anything.
+ * @format: list of types of arguments.
+ *
+ */
+
+void print_all(const char * const format, ...)
+{
+	fmts types[] = {
+		{"c", add_char},
+		{"i", add_int},
+		{"f", add_float},
+		{"s", add_str},
+		{NULL, NULL}
+	};
+
+	int i, j = 0;
+	va_list list;
+	char *ptr = "";
+
+	va_start(list, format);
+
+	while (format && format[j])
+	{
+		i = 0;
+
+		while (types[i].f != NULL)
+		{
+			if (types[i].type[0] == format[j])
+			{
+				printf("%s", ptr);
+				types[i].f(list);
+				ptr = ", ";
+				break;
+			}
+			i++;
+		}
+		j++;
+	}
+	printf("\n");
+	va_end(list);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,26 @@
 #include <string.h>
 #include <stdarg.h>
 
+/**
+ * struct type_fmt - struct format.
+ *
+ * @type: format.
+ * @f: The function associated.
+ */
+typedef struct type_fmt
+{
+	char *type;
+	void (*f)(va_list);
+} fmts;
+
+/* Print prototypes */
+
+void add_char(va_list list);
+void add_int(va_list list);
+void add_float(va_list list);
+void add_str(va_list list);
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
