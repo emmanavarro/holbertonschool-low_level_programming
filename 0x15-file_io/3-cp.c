534179c11bf591b7e90a4ebd640a7ae625c6463d
Emma Navarro <elnavarro55@gmail.com>
2020-04-01 03:32:51 +0000

    Fix betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,9 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from < 0)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	}
 	fd_to = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (fd_to < 0)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
