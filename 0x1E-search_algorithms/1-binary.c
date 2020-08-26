4c61fe7bfa49f79af3b353fa6de916791074aacf
Emma Navarro <elnavarro55@gmail.com>
2020-08-26 17:19:09 +0000

    Fix stop condition in for loop

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -17,7 +17,7 @@ int binary_search(int *array, size_t size, int value)
 	while (begin <= end)
 	{
 		printf("Searching in array: ");
-		for (i = begin; i <= end; i++)
+		for (i = begin; i < end; i++)
 		{
 			printf("%d, ", array[i]);
 		}
