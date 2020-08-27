e589bc274520f610c9473f8980cec59418063e4b
emmanavarro <elnavarro55@gmail.com>
2020-08-27 12:39:25 -0500

    searches for a value in a sorted array of integers using the Interpolation search algorithm

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+/**
+  * interpolation_search - search the first ocurrency of a value in the array
+  * @array: set of numbers
+  * @size: size of the array
+  * @value: value to search
+  * Return: return the first index located, otherwise -1
+  */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t left = 0, right = size - 1, pos = 0;
+
+	if (!array)
+		return (-1);
+	pos = left + (((double)(right - left) / (array[right] - array[left])) * (value - array[left]));
+	if (pos >= size)
+		printf("Value checked array[%d] is out of range\n", (int)pos);
+	while ((array[right] != array[left]) && (value >= array[left]) && (value <= array[right]))
+	{
+		pos = left + (((double)(right - left) / (array[right] - array[left])) * (value - array[left]));
+		printf("Value checked array[%d] = [%d]\n", (int)pos, array[pos]);
+
+		if (array[pos] < value)
+			left = pos + 1;
+		else if (array[pos] > value)
+			right = pos - 1;
+		else
+			return (pos);
+	}
+	if (array[left] == value)
+		return (left);
+	return (-1);
+}
